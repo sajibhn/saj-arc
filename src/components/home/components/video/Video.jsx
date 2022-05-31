c5cba292c211d3d6bd5f71b0f01e33cf8b99d1cf
Sajib Hossain <sajibhossain1185@gmail.com>
2022-05-31 18:46:10 +0600

    made video section dynamic

--- src/components/home/components/video/Video.jsx
+++ src/components/home/components/video/Video.jsx
@@ -2,14 +2,14 @@ import React, { useState } from 'react'
 import ModalVideo from 'react-modal-video'
 import 'react-modal-video/scss/modal-video.scss';
 import { BsPlay } from 'react-icons/bs';
-
+import { url } from '../../../../data/videoData';
 
 const Video = () => {
     const [isOpen, setOpen] = useState(false)
     return (
         <section className="video section">
             <div className="section__container container">
-                <ModalVideo channel='youtube' autoplay isOpen={isOpen} videoId="oyA7Ucnmje4" onClose={() => setOpen(false)} />
+                <ModalVideo channel='youtube' autoplay isOpen={isOpen} videoId={url} onClose={() => setOpen(false)} />
 
                 <button className="btn-primary video__btn" onClick={() => setOpen(true)}>
                     <BsPlay />
