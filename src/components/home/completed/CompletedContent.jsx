56ad2fc9fea10ecc5a166d7e41d957833b99c232
Sajib Hossain <sajibhossain1185@gmail.com>
2022-05-31 16:23:35 +0600

    made completed section dynamic

--- src/components/home/completed/CompletedContent.jsx
+++ src/components/home/completed/CompletedContent.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import CountUp from 'react-countup';
+import VisibilitySensor from 'react-visibility-sensor';
+import { completedData } from '../../../data/completedData';
 
 const CompletedContent = () => {
+
+    const [datas, setDatas] = useState(completedData);
+
     return (
         <div className="completed__content grid">
-            <div className="completed__content__data">
+            {/* <div className="completed__content__data">
                 <h3>57</h3>
                 <h4>Web development</h4>
             </div>
@@ -18,7 +24,19 @@ const CompletedContent = () => {
             <div className="completed__content__data">
                 <h3>38</h3>
                 <h4>Web development</h4>
-            </div>
+            </div> */}
+            <VisibilitySensor partialVisibility >
+                {({ isVisible }) => (
+                    datas.map((data) => {
+                        return (
+                            <div className="completed__content__data" key={data.id}>
+                                <h3>{isVisible ? <CountUp end={data.number} /> : null}</h3>
+                                <h4>{data.title}</h4>
+                            </div>
+                        )
+                    })
+                )}
+            </VisibilitySensor>
         </div>
     )
 }
---
+++ src/data/completedData.js
@@ -0,0 +1,22 @@
+export const completedData = [
+    {
+        id: 1,
+        number: 12,
+        title: "YEARS EXPERIENCE"
+    },
+    {
+        id: 2,
+        number: 36,
+        title: 'EXPERT PARTNERS'
+    },
+    {
+        id: 3,
+        number: 87,
+        title: 'HAPPY CUSTOMERS'
+    },
+    {
+        id: 4,
+        number: 10,
+        title: 'ACTIVE PROJECTS'
+    },
+];
