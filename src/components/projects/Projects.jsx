a02747b0268bc29a58d145c6825901e27bdf6466
Sajib Hossain <sajibhossain1185@gmail.com>
2022-06-01 13:54:52 +0600

    made all project section

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import AllProjects from './all projects/AllProjects'
 import FeaturedProject from './featured/FeaturedProject'
 
 const Projects = () => {
     return (
         <>
             <FeaturedProject />
+            <AllProjects />
         </>
     )
 }
---
+++ src/components/projects/all projects/AllProjects.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { architectureData } from '../../../data/architectureData'
+
+const AllProjects = () => {
+    const [datas, setDatas] = useState(architectureData)
+    return (
+        <section className="section all__projects">
+            <div className="all__projects__container container">
+                <h2 className="all__projects__title">
+                    all projects
+                </h2>
+                <div className="all__projects__content grid">
+                    {datas.map((data) => {
+                        const { id, image, title, category } = data
+                        return (
+                            <div className="all__projects__content__data project__data" key={id}>
+                                <img src={image} alt="" />
+                                <div className="all__projects__content__data__body project__data__body">
+                                    <h3>{title}</h3>
+                                    <p>{category}</p>
+                                    <a href="/">view project</a>
+                                </div>
+                            </div>
+                        )
+                    })}
+
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default AllProjects
