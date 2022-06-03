5b1bb40f7db027f86b851c7a1221d75c59233249
Sajib Hossain <sajibhossain1185@gmail.com>
2022-06-03 18:29:06 +0600

    optimized css and header

--- src/data/architectureData.js
+++ src/data/architectureData.js
@@ -40,7 +40,7 @@ export const architectureData = [
         title: 'city villa hall',
         description: 'Heading with text.The relationship between geometry and architectural design are described and discussed along some examples. Geometry is the fundamental science of forms and their order. Geometric figures, forms and transformations build the material of architectural design. In the history of architecture geometric rules based on the ideas of proportions and symmetries formed fixed tools for architectural design. The relationship between geometry and architectural design are described. <br /> <br /> The relationship between geometry and architectural design are described and discussed along some examples. Geometry is the fundamental science of forms and their order. Geometric figures, forms and transformations build the material of architectural design.In the history of architecture.',
         client: 'insight studio',
-        date: '14 Seiptembor 2018',
+        date: '14 Septembor 2018',
         services: 'Design, Art Direction, Website'
     },
     {
