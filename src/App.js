28f186cedaab4e385bd728720a935d458d0a9759
Sajib Hossain <sajibhossain1185@gmail.com>
2022-06-03 11:39:40 +0600

    installed react router and created routes

--- src/App.js
+++ src/App.js
@@ -8,18 +8,25 @@ import Services from './components/services/Services'
 import About from './components/about/About'
 import SinglePost from './components/global/single post/SinglePost'
 import Contact from './components/contact/Contact'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <>
-      <Header />
-      {/* <Home /> */}
-      {/* <Projects /> */}
-      {/* <Services /> */}
-      {/* <About /> */}
-      {/* <SinglePost /> */}
-      <Contact />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/featured/:id' element={<SinglePost />} />
+          <Route path='/projects/:id' element={<SinglePost />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+
     </>
   )
 }
